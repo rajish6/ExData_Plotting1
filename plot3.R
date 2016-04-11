4e37014b8dd74abbda53c32be43f390d834f23b7
rajish6 <rajish6@yahoo.com>
2016-04-10 21:55:27 -0400

    Added files via upload

---
+++ plot3.R
@@ -0,0 +1,32 @@
+# As the data set "household_power_consumption.txt" is not part of the repo, make
+# sure that this file is just in the parent folder containing this repo.
+# 2 days = 2880 minutes, First read with 2880 rows after skipping 66637 rows to reach February 1st, 2007
+# Next read the same table with one row to obtain names
+# Date & Time is merged to obtain one column and that column is added 
+# and formatted
+# Plots are produced per question asked
+
+hh_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", skip = 66637, nrows = 2880)
+
+hh_data_names <- read.table("household_power_consumption.txt", header = TRUE, sep= ";", nrows = 1)
+
+names(hh_data) <- names(hh_data_names)
+
+date_time <- paste(hh_data$Date, hh_data$Time)
+hh_data_new$date_time <- as.data.frame(date_time)
+hh_data_new <- cbind(date_time, hh_data)
+hh_data_new$date_time <- strptime(hh_data_new$date_time, "%d/%m/%Y %H:%M")
+
+png(filename = "./ExData_Plotting1/plot3.png", width = 480, height = 480, units = "px")
+with(hh_data_new, plot(date_time, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering"))
+with(hh_data_new, points(date_time, Sub_metering_1, type = "l"))
+with(hh_data_new, points(date_time, Sub_metering_2, type = "l", col = "red"))
+with(hh_data_new, points(date_time, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
+
+
+
+
+                         
